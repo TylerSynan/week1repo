b4b103a9a251db12e511717a3c13a02078142960
TylerSynan <tylersynan@gmail.com>
2021-09-25 17:27:33 -0700

    first real commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello To My Class!");
+    System.out.println("My instructor is George.");
+    
+    int shoeSize;
+    shoeSize = 11;
+    shoeSize = shoeSize + 1;
+    System.out.println("Shoe Size: " + shoeSize);
+    
+    //fehrenheit to celcius
+    //
+    int temperature = 65;
+    int celcius = (temperature - 32) * 5 / 9;
+    System.out.println(temperature + "F => " + celcius + "C");
+  }
+
+}
